7fa9fb1a4d41c142fdb59b0363c5ba2bf9191707
Victor Saly <Victor.Saly@storm-technologies.com>
2021-02-10 22:20:57 +0000

    feat: level, operators & locastorage for stars

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,22 @@
         </ion-chip>
       </ion-toolbar>
     </ion-header>
-
+    <ion-item>
+          <ion-label>Level</ion-label>
+          <ion-select interface="popover" :value="selectedLevel" @ionChange="selectedLevel=$event.target.value">
+            <ion-select-option value="beginner">Beginners</ion-select-option>
+            <ion-select-option value="medium">Medium</ion-select-option>
+            <ion-select-option value="expert">Expert</ion-select-option>
+          </ion-select>
+    </ion-item>
+    <ion-item>
+          <ion-label>Operator</ion-label>
+          <ion-select interface="popover" :value="selectedOperator" @ionChange="selectedOperator=$event.target.value; askQuestion">>
+            <ion-select-option value="times">multiply (x)</ion-select-option>
+            <ion-select-option value="plus">Addition (+)</ion-select-option>
+            <ion-select-option value="minus">Substraction (-)</ion-select-option>
+          </ion-select>
+    </ion-item>
     <ion-content :fullscreen="true">
       <BotFace :botState="botState" :isPlayMode="isPlayMode" :text="speech_phrases" v-on:ask_question="askQuestion" />
     </ion-content>
@@ -26,16 +41,19 @@ import {
   IonChip,
   IonLabel,
   IonIcon,
-  toastController 
+  toastController,
+  IonItem,
+  IonSelect,
+  IonSelectOption
 } from "@ionic/vue";
 import BotFace from "@/components/BotFace.vue";
 import {
   AudioConfig,
   SpeechConfig,
-  SpeechRecognizer
+  SpeechRecognizer,
+  PhraseListGrammar
 } from "microsoft-cognitiveservices-speech-sdk";
-// eslint-disable-next-line no-unused-vars
-// import axios from 'axios';
+import axios from 'axios';
 import { star } from 'ionicons/icons';
 export default {
   name: "Math",
@@ -48,18 +66,27 @@ export default {
     IonPage,
     IonChip,
     IonLabel,
-    IonIcon
+    IonIcon,
+    IonItem,
+    IonSelect,
+    IonSelectOption
   },
   data() {
     return {
       isLoading: true,
       isTalking: false,
       isListening: false,
+      isComputing: false,
       isQuery: false,
       isPlayMode:true,
       text: "",
+      selectedLevel:"beginner",
+      selectedOperator:"times",
       speech_phrases: "",
       synth: window.speechSynthesis,
+      encourage_phases:["Well done!","Great work!","Good job!", "Keep up the good work!", "You've really got this!"],
+      correct_phases:["That's correct","That's right","That's true", "Flawless", "Perfect"],
+      incorrect_phases:["Not quite","Maybe next try","I'm sure you can get the next one right", "Incorrect"],
       voiceList: [],
       greetingSpeech: new window.SpeechSynthesisUtterance(),
       audioConfig: null,
@@ -69,7 +96,8 @@ export default {
       number1 : 2,
       number2 : 3,
       // eslint-disable-next-line no-undef
-      token: token,
+      token: null,
+      tokenUrl: process.env.VUE_APP_TOKEN_URL,
       previousPosition: -1,
       stars: 0,
     };
@@ -80,11 +108,20 @@ export default {
         return "speaking";
       } else if (this.isListening) {
         return "listening";
-      } else {
+      } else if (this.isComputing) {
+        return "computing";
+      }
+      else {
         return "thinking";
       }
     },
     expectedResultAsNumber() {
+      if (this.selectedOperator == "plus") {
+            return this.number1 + this.number2;
+      }
+      if (this.selectedOperator == "minus") {
+         return this.number1 - this.number2;
+      }
       return this.number1 * this.number2;
     }
   },
@@ -105,9 +142,71 @@ export default {
        this.isQuery = true;
        this.isPlayMode = false;
        this.speech_phrases = "";
-       this.number2 = this.getRandomInt(1, 12);
-       this.number1 = this.getRandomInt(1, 12);
-       this.text = "What's " + this.number1 + " times " + this.number2 + "?";
+       var min1 = 1;
+       var max1 = 12;
+       var min2 = 1;
+       var max2 = 12;
+       if (this.selectedOperator == "plus")
+       {
+         max1 = 50;
+         max2 = 50;
+
+         if (this.selectedLevel == "medium"){
+            max1 = 100;
+            max2 = 100;
+         }
+
+         if (this.selectedLevel == "expert"){
+            max1 = 1000;
+            max2 = 1000;
+         }
+            
+       }
+
+       if (this.selectedOperator == "minus")
+       {
+          min1 = 10;
+          max1 = 20;
+          min2 = 1;
+          max2 = 10; 
+
+          if (this.selectedLevel == "medium"){
+            min1 = 20;
+            max1 = 100;
+            min2 = 1;
+            max2 = 20;   
+          }
+
+         if (this.selectedLevel == "expert"){
+            min1 = 100;
+            max1 = 500;
+            min2 = 1;
+            max2 = 100;
+         }
+       }
+
+       if (this.selectedOperator == "times")
+       {
+          
+          if (this.selectedLevel == "medium"){
+            min1 = 3;
+            max1 = 12;
+            min2 = 3;
+            max2 = 12;  
+          }
+
+         if (this.selectedLevel == "expert"){
+            min1 = 5;
+            max1 = 20;
+            min2 = 5;
+            max2 = 20; 
+         }
+       }
+       this.number1 = this.getRandomInt(min1, max1);
+       this.number2 = this.getRandomInt(min2, max2);
+       
+       
+       this.text = "What's " + this.number1 + " " + this.selectedOperator + " " + this.number2 + "?";
        this.speak();
     },
     /**
@@ -121,7 +220,9 @@ export default {
       this.synth.speak(this.greetingSpeech);
     },
     listen() {
-      this.isListening = true;
+      this.showToast("Connecting...", "warning");
+      this.isComputing = true;
+      this.isListening = false;
       this.audioConfig = AudioConfig.fromDefaultMicrophoneInput();
       var sc = SpeechConfig.fromAuthorizationToken(
         // eslint-disable-next-line no-undef
@@ -134,6 +235,9 @@ export default {
         this.speechConfig,
         this.audioConfig
       );
+
+      var phraseListGrammar = PhraseListGrammar.fromRecognizer(this.speechRecording);
+      phraseListGrammar.addPhrase(String(this.expectedResultAsNumber));
       this.listenForSpeechRecordingEvents();
     },
     /**
@@ -141,12 +245,20 @@ export default {
      */
     listenForSpeechRecordingEvents() {
       const self = this;
+
+      // Signals that a new session has started with the speech service
+      this.speechRecording.speechStartDetected = function () {
+        console.log('speechStartDetected');
+          self.showToast("I'm Listening", "success");
+          self.isListening = true;
+          self.isComputing = false;
+      };
+      
       this.speechRecording.recognizeOnceAsync(
         function (result) {
-          // console.log(result);
-          // console.log('result recognizeOnceAsync', result);
           self.validateSpeechRecording(result.text);
           self.isListening = false;
+          self.isComputing = false;
           self.speechRecording.close();
           self.speechRecording = null;
         },
@@ -181,20 +293,29 @@ export default {
         // console.log('boundary', e);
 
         if (e.name == "word") {
-          var word = this.getWordAt(this.text, e.charIndex);
+          var word = this.getWordAt(this.text, e.charIndex).toLowerCase();
           // console.log(word);
           if (word == "times") {
             word = "x";
           }
+
+          if (word == "plus") {
+            word = "+";
+          }
+
+          if (word == "minus") {
+            word = "-";
+          }
+
           this.speech_phrases += word + " ";
         }
       };
     },
     validateSpeechRecording(recordedText) {
       // Perform type conversions.
-      recordedText = String(recordedText);
+      recordedText = String(recordedText == undefined ? '(silent)' : recordedText);
       
-      this.showToast("Your response is : " + recordedText);
+      this.showToast("Your response is : " + recordedText, "secondary");
 
       if (recordedText != undefined && recordedText.length > 0) {
             var recordedTextAsNumber = recordedText.match(/\d/g);
@@ -203,13 +324,15 @@ export default {
             }
             if (recordedTextAsNumber == this.expectedResultAsNumber) {
                 this.stars++;
-                this.text = "Correct, well done!."
+                localStorage.stars = this.stars;
+                this.text = this.correct_phases[Math.floor(Math.random()*this.correct_phases.length)] + ", " + this.encourage_phases[Math.floor(Math.random()*this.encourage_phases.length)] + "; You earned " + this.stars + " star" + (this.stars == 1 ? "" : "s") + "."
             } else {
-                this.text ='Incorrect, the correct answer is ' + this.expectedResultAsNumber + ".";
+                this.text = this.incorrect_phases[Math.floor(Math.random()*this.incorrect_phases.length)] + ', the correct answer is ' + this.expectedResultAsNumber + ".";
             }
            
         }else{
                 this.text ='Not answer found.';
+                this.showToast("Not answer found.", "danger");
         }
         this.greetingSpeech.text = this.text;
         this.synth.speak(this.greetingSpeech);
@@ -221,6 +344,7 @@ export default {
 
       // console.log('previousPosition', this.previousPosition)
       if (this.previousPosition == pos){
+        this.isTalking = false;
         return "";
       }
       this.previousPosition = pos;
@@ -233,6 +357,7 @@ export default {
         if (!this.isQuery){
           this.isPlayMode = true;
         }
+        this.isTalking = false;
         return str.slice(left);
       }
       
@@ -255,46 +380,16 @@ export default {
         console.log('No mic for you!')
       });
 
-    // wait for voices to load
-    // I can't get FF to work without calling this first
-    // Chrome works on the onvoiceschanged function
-    this.voiceList = this.synth.getVoices();
-
-    if (this.voiceList.length) {
-      this.isLoading = false;
-    }
-
-    this.synth.onvoiceschanged = () => {
-      this.voiceList = this.synth.getVoices();
-      // give a bit of delay to show loading screen
-      // just for the sake of it, I suppose. Not the best reason
-      setTimeout(() => {
-        this.isLoading = false;
-      }, 800);
-    };
+    
     this.listenForSpeechEvents();
 
+    if (localStorage.stars) {
+      this.stars = localStorage.stars;
+    }
 
-  //    axios
-  //  .post(
-  //    'https://uksouth.api.cognitive.microsoft.com/sts/v1.0/issueToken',
-  //    null,
-  //    {
-  //      headers: {
-  //        "Ocp-Apim-Subscription-Key": "a66a133c602c4a4c9a4eec0735090e2c",
-  //        "Content-Type": "application/json",
-  //      }
-  //    }
-  //  )
-  //  .then((response) => {
-  //    console.log("Status text: " + response.status);
-  //    console.log("Status text: " + response.statusText);
-  //    console.log(response.data);
-  //    this.token = response.data;
-  //  })
-  //  .catch((err) => {
-  //    throw err;
-  //  });
+    axios.get(this.tokenUrl).then((response) => {      
+        this.token = response.data;
+    });
   },
   setup() {
     return {
