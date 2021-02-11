cc544696433f278b05ab2e3529eed86be03f70d3
Victor Saly <Victor.Saly@storm-technologies.com>
2021-02-11 08:09:46 +0000

    chore: prefers dark force matchMedia

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -394,6 +394,19 @@ export default {
     axios.get(this.tokenUrl).then((response) => {      
         this.token = response.data;
     });
+
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    toggleDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+
+    // Add or remove the "dark" class based on if the media query matches
+    function toggleDarkTheme(shouldAdd) {
+      document.body.classList.toggle('dark', shouldAdd);
+    }
   },
   setup() {
     return {
