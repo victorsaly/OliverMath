760414523ff7fa87b4eb0d847cc197606aa034d8
Victor Saly <Victor.Saly@storm-technologies.com>
2021-02-11 10:15:15 +0000

    chore: update face skin

--- src/components/BotFace.vue
+++ src/components/BotFace.vue
@@ -156,8 +156,9 @@ p.thought:after {
   margin-right: 0%;
   width: 100%;
   height: 100%;
-  border: 0.4em solid #FFF;
+  border: 0.4em solid #ccc;
   border-radius: 1.5em;
+  background-color: #000;
   /*border : 1px solid red;*/
 }
 
