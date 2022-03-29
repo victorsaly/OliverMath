637f24e6abf236854c6f7361eb0a6a149ac85f09
victorsaly <info@victorsaly.com>
2022-03-29 22:03:46 +0100

    chore: boost version 1.0.10

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Math Game V1.0.9</ion-title>
+        <ion-title>Math Game V1.0.10</ion-title>
         <ion-chip slot="end">
           <ion-icon :icon="star" color="dark"></ion-icon>
           <ion-label>{{ stars }}</ion-label>
