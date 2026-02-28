1945546718675cbdc877c55c6f98f6a9be51883a
victorsaly <victorsaly@users.noreply.github.com>
2026-02-28 14:57:19 +0000

    deploy: a0acaa2975deb58b052da087ca0ed448caef06c0

--- assets/p-DyHPCMyn-ClLE1H35.js
+++ assets/p-DyHPCMyn-ClLE1H35.js
@@ -1,3 +1,3 @@
-import{G as i,H as a,J as c,K as d,L as l}from"./ionic-core-CmBygKQQ.js";import"./vue-vendor-CG_fTKmE.js";/*!
+import{H as i,J as a,K as c,L as d,M as l}from"./ionic-core-QKhxqlE1.js";import"./vue-vendor--Omo8iNx.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{i(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=a(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
